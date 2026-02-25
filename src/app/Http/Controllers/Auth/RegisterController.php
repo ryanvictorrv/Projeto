810b801e44f2bc68fd16341553b7852d063af702
ryan victor <ryanv8434@gmail.com>
2026-02-25 17:37:24 -0300

    Adiciona tela de cadastro e login com verificação de email existente

---
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Auth;  // muito importante!
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    // Mostra o formulário
+    public function show()
+    {
+        return view('register');
+    }
+
+    
+    // Processa o cadastro
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+            // Verifica se o email já existe
+        $userExists = User::where('email', $request->email)->first();
+
+        if ($userExists) {
+        // Se já existe, redireciona para login com mensagem
+            return redirect('/login')->with('info', 'Email já cadastrado! Faça login.')-> with('email', $request->email);
+        }
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect('/login')->with('success', 'Cadastro realizado com sucesso!');
+    }
+}
